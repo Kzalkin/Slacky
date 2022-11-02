ff77f39faeb6d6002af57196bbcc0a2122b7c1c0
Eglecerio Malunes III <egleceriom425@gmail.com>
2022-11-02 17:55:29 +0800

    adds password and password confirmation validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,7 @@ class User < ApplicationRecord
 
     validates :email, presence: true, uniqueness: true
     validates :email, format: {with: URI::MailTo::EMAIL_REGEXP}
+    validates :password, presence: true
     validates :password, length: {minimum: 6}
+    validates :password_confirmation, presence: true
 end
