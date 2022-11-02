5f5f97d0f7ce8f039d10385bf000fd27a7d11a7c
Eglecerio Malunes III <egleceriom425@gmail.com>
2022-11-02 18:17:50 +0800

    modifies json output for success and failure user registration

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -4,9 +4,9 @@ class Api::V1::AuthenticationController < ApplicationController
         @user = User.new(user_params)
 
         if @user.save
-            render json: {message: "User successfuly created", status: :created}
+            render json: {status: "Success", message: "User successfuly created"}, status: :created
         else
-            render jsonapi_errors: @user.errors
+            render json: {status: "Error", errors: @user.errors}, status: :unprocessable_entity
         end
     end
 
