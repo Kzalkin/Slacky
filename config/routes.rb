21ee4f992b9679ff8f430045d4fd67a484e76045
Eglecerio Malunes III <egleceriom425@gmail.com>
2022-11-02 17:56:52 +0800

    adds authentication route for user registration

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: %i[index show update create destroy]
+
+      post 'auth', to: 'authentication#registration'
     end
   end
 end
