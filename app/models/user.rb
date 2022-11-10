8aa66a117b4cae3f0b8d8434fc0c38aa695246e5
Eglecerio Malunes III <egleceriom425@gmail.com>
2022-11-10 21:31:42 +0800

    removes direct message association

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,10 +9,4 @@ class User < ApplicationRecord
     validates :password, presence: true
     validates :password, length: {minimum: 6}
     validates :password_confirmation, presence: true
-
-    has_many :received_direct_messages, foreign_key: :recipient_id, class_name: "DirectMessage"
-    has_many :messages, through: :received_direct_messages, source: :message
-
-    has_many :sent_direct_messages, foreign_key: :sender_id, class_name: "DirectMessage"
-    has_many :sent_messages, through: :sent_direct_messages, source: :message
 end
