a031cb1db2d3d8eba71901cb12311d5c003d4497
Eglecerio Malunes III <egleceriom425@gmail.com>
2022-11-25 14:36:10 +0800

    drops userschannels table

---
+++ db/migrate/20221125063513_drop_table_userschannels.rb
@@ -0,0 +1,5 @@
+class DropTableUserschannels < ActiveRecord::Migration[7.0]
+  def change
+    drop_table :userschannels
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_10_132531) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_25_063513) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -33,15 +33,4 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_10_132531) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "userschannels", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "channel_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["channel_id"], name: "index_userschannels_on_channel_id"
-    t.index ["user_id"], name: "index_userschannels_on_user_id"
-  end
-
-  add_foreign_key "userschannels", "channels"
-  add_foreign_key "userschannels", "users"
 end
