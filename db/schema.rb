3129d58bc41d490268f8391029efca636d2fb78c
Eglecerio Malunes III <egleceriom425@gmail.com>
2022-11-25 14:41:54 +0800

    adds user and channel references to messages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_25_063513) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_25_064008) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_25_063513) do
     t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "channel_id", null: false
+    t.bigint "user_id", null: false
+    t.index ["channel_id"], name: "index_messages_on_channel_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -33,4 +37,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_25_063513) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "messages", "channels"
+  add_foreign_key "messages", "users"
 end
