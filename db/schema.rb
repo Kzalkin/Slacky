83eb3fd018def200ad7a3948395cda0051e9d89d
Eglecerio Malunes III <egleceriom425@gmail.com>
2022-11-28 19:23:00 +0800

    adds membership model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_25_064243) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_28_111700) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_25_064243) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "memberships", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "channel_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["channel_id"], name: "index_memberships_on_channel_id"
+    t.index ["user_id"], name: "index_memberships_on_user_id"
   end
 
   create_table "messages", force: :cascade do |t|
@@ -37,6 +46,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_25_064243) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "memberships", "channels"
+  add_foreign_key "memberships", "users"
   add_foreign_key "messages", "channels"
   add_foreign_key "messages", "users"
 end
