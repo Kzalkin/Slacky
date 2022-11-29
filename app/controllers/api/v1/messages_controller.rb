f3028f5da26cef6364eec76ea95744fa3f90efd7
Eglecerio Malunes III <egleceriom425@gmail.com>
2022-11-29 08:01:54 +0800

    adds channel message creation

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,2 +1,16 @@
 class Api::V1::MessagesController < ApplicationController
+    def create
+        @channel = params[:receiver_class].constantize.find(params[:receiver_id])
+        @user = @channel.members.find(request.headers['uid'])
+
+        @message = @user.messages.create!(body: message_params[:body], channel_id: @channel.id)
+
+        render jsonapi: @message, include: [:user, :channel]
+    end
+
+    private
+
+    def message_params
+        params.permit(:body)
+    end
 end
