235c12946fd70cbbb500f866104f96b1a3aa1fc8
Eglecerio Malunes III <egleceriom425@gmail.com>
2022-11-30 03:55:12 +0800

    adds create_private_room method in channel model

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,11 +1,9 @@
 class Api::V1::MessagesController < ApplicationController
     before_action -> {check_params("receiver_class", "receiver_id")}, only: :create
+    before_action :set_channel, only: :create
 
     def create
-        @channel = params[:receiver_class].constantize.find(params[:receiver_id])
-        @user = @channel.members.find(request.headers['uid'])
-
-        @message = @user.messages.new(body: message_params[:body], channel_id: @channel.id)
+        @message = current_user.messages.new(body: message_params[:body], channel_id: @channel.id)
 
         if @message.save
             render jsonapi: @message, include: [:user, :channel]
@@ -25,5 +23,20 @@ class Api::V1::MessagesController < ApplicationController
         inputs.each {|param| errors.push(param) unless params.has_key?(param.to_sym)}
 
         render json: {status: "Error", error: "No #{errors.join(", ")} found"} if errors.length > 0
+    end
+
+    def create_name(user1, user2)
+        users = [user1, user2].sort
+        "private_#{users.first.id}_#{users.last.id}"
+    end
+
+    def set_channel
+        if params[:receiver_class] == "User"
+            @user = User.find(params[:receiver_id])
+            @name = create_name(current_user, @user)
+            @channel = Channel.find_by(name: @name) || Channel.create_private_room([@user, current_user], @name)
+        else
+            @channel = params[:receiver_class].constantize.find(params[:receiver_id]) if params[:receiver_class] == "Channel"
+        end
     end
 end
--- app/models/channel.rb
+++ app/models/channel.rb
@@ -4,4 +4,12 @@ class Channel < ApplicationRecord
     has_many :messages, dependent: :destroy
     has_many :membership, dependent: :destroy
     has_many :members, through: :membership, source: :user
+
+    def self.create_private_room(users, name)
+        channel = Channel.create(name: name, is_private: true)
+        users.each do |user|
+            Membership.create(user_id: user.id, channel_id: channel.id)
+        end
+        channel
+    end
 end
