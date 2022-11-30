b87543b74b24625ef6b4b418bf82920322c7ab33
Eglecerio Malunes III <egleceriom425@gmail.com>
2022-12-01 07:50:22 +0800

    removes unused user routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :users, only: %i[index show update create destroy]
+      resources :users, only: %i[index]
       resources :channels, only: %i[index create show destroy]
       resources :messages, only: %i[index create]
 
